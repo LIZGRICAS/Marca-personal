9fbe0ce42aef13036885bc171d78b6fb29433fd7
lgrisalesc <lizgricas@gmail.com>
2025-02-16 21:38:14 -0500

    actualizacion de banner principal con nuevas  animaciones, codigo y estilos en 3d

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+// translate mouse movement into 3D rotation
+window.addEventListener("mousemove", e => {
+    w = window.innerWidth;
+    h = window.innerHeight;
+  
+    const translateX  =  e.pageX * (106 / w);
+    const translateXLayer  =  e.pageX * (16 / w);
+  
+    document.getElementById(
+    "card").
+    style.transform = `translateX(${translateX }px)`;
+    document.getElementById(
+        "layer").
+        style.transform = `translateX(${translateXLayer }px)`;
+  });
+  
+  // translate touch movement into 3D rotation
+  window.addEventListener("touchmove", e => {
+    w = window.innerWidth;
+    h = window.innerHeight;
+  
+    const translateX =  e.changedTouches[0].pageX * (100 / w);
+    const translateXLayer =  e.changedTouches[0].pageX * (20 / w);
+  
+    document.getElementById(
+    "card").
+    style.transform = `translateX(${translateX}px)`;
+    document.getElementById(
+        "layer").
+        style.transform = `translateX(${translateXLayer}px)`;
+  });
